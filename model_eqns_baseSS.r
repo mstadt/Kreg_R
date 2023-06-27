455637965cc28a2e6d276ed755490b7838d0b2ef
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2023-06-27 12:39:05 -0400

    run morris sensitivity analysis

---
+++ model_eqns_baseSS.r
@@ -0,0 +1,166 @@
+model_eqns_baseSS <- function(Time, State, Pars) {
+
+    # variable names
+    # amt_gut <- y[1]
+    # amt_plas <- y[2]
+    # amt_inter <- y[3]
+    # amt_muscle <- y[4]
+
+    dydt <- c()
+    with(as.list(c(State, Pars)), {
+
+        # set parameters that are fixed (not in Morris)
+        PhiKinSS <- 70/1440
+        kgut <- 0.01
+        MKgutSS <- (0.9 * PhiKinSS) / kgut
+        KMuscleBase = 130
+        KECF_base <- 4.2
+        VMAX = 130
+        KM = 1.4
+        NKAbase = (VMAX*KECF_base)/(KM + KECF_base)
+        Pmuscle <- NKAbase/(KMuscleBase - KECF_base)
+        V_plasma = 4.5
+        V_inter = 10
+        V_muscle = 24
+        m_K_ALDO = 0.5
+        ALD_eq = 85
+        #Phi_Kin_ss = PhiKinSS,
+        #kgut = kgut
+        fecal_exc = 0.1
+        P_ECF = 0.3
+        #FF = 0.250274
+        amt_gutSS = MKgutSS
+        Kecf_base = KECF_base
+        GFR = 0.125
+        etapsKreab = 0.92
+        dtKsec_eq = 0.041
+        A_dtKsec = 0.3475
+        B_dtKsec = 0.23792
+        cdKsec_eq = 0.0022
+        A_cdKsec = 0.161275
+        B_cdKsec = 0.410711
+        A_cdKreab = 0.499994223625298
+        A_insulin = 0.999789
+        B_insulin = 0.6645
+        t_insulin_ss = 270
+        Vmax = VMAX
+        Km = KM
+        P_muscle = Pmuscle
+
+        # simulation settings
+        SS = 1
+        do_FF = 1
+        do_insulin = 1
+        MKX = 0
+        Kintake = 70/1440
+        # concentrations
+        Kplas = amt_plas/V_plasma
+        Kinter = amt_inter/V_inter
+        Kmuscle = amt_muscle/V_muscle
+        K_ECFtot = (amt_plas + amt_inter)/(V_plasma + V_inter)
+
+        # ALD
+        N_al = exp(m_K_ALDO * (K_ECFtot - Kecf_base))
+        C_al = N_al * ALD_eq
+
+
+        # amt_gut (Gut K)
+        if (SS) {
+            Phi_Kin = Phi_Kin_ss
+        } else {
+            Phi_Kin = Kintake
+        }
+        K_intake = (1 - fecal_exc) * Phi_Kin
+        Gut2plasma = kgut * amt_gut
+        # d(amt_gut)/dt
+        dgut <- K_intake - Gut2plasma
+
+        # amt_plas (Plasma K)
+        Plas2ECF = P_ECF*(Kplas - Kinter)
+
+        # GI FF effect
+        if (do_FF) {
+            temp = FF * (amt_gut - amt_gutSS) + 1
+            gamma_Kin = max(1,temp)
+        } else {
+            gamma_Kin = 1
+        }
+
+        # renal K handling
+        filK = GFR * Kplas
+        psKreab = etapsKreab * filK
+
+        # ALD impact
+        gamma_al = A_dtKsec * C_al^B_dtKsec
+        lambda_al = A_cdKsec * C_al^B_cdKsec
+
+        eta_dtKsec = gamma_al * gamma_Kin
+        dtKsec = dtKsec_eq * eta_dtKsec
+
+        eta_cdKsec = lambda_al
+        cdKsec = cdKsec_eq * eta_cdKsec
+
+        eta_cdKreab = 1
+        dtK = filK - psKreab + dtKsec
+        cdKreab = dtK * A_cdKreab * eta_cdKreab
+
+        UrineK = dtK + cdKsec - cdKreab
+
+        # d(amt_plas)/dt
+        dplas <- Gut2plasma - Plas2ECF - UrineK
+
+        # amt_inter (Interstitial K)
+        rho_al = (66.4 + 0.273 * C_al)/89.6050
+
+        # insulin
+        if (do_insulin){
+            if (SS) {
+                t_insulin = t_insulin_ss
+            } else {
+                t_insulin = t
+            }
+            C_insulin = get_ins(t_insulin)
+            L = 100
+            x0 = 0.5381
+            k = 1.069
+            ins_A = A_insulin
+            ins_B = 100 * B_insulin
+            temp = (ins_A*(L/(1+exp(-k*(log10(C_insulin)-log10(x0)))))+ ins_B)/100
+            rho_insulin = max(1.0, temp)
+        } else {
+            # set insulin to SS amount
+            C_insulin = 22.6/1000
+            rho_insulin = 1
+        }
+
+        eta_NKA = rho_insulin * rho_al
+
+        Inter2Muscle = eta_NKA * ((Vmax * Kinter)/(Km + Kinter))
+        Muscle2Inter = P_muscle * (Kmuscle - Kinter)
+
+        # d(amt_inter)/dt
+        dinter <- Plas2ECF - Inter2Muscle + Muscle2Inter
+
+        # amt_muscle (intracellular K)
+        # d(amt_muscle)/dt
+        dmuscle <- Inter2Muscle - Muscle2Inter
+
+        return(list(c(dgut, dplas, dinter, dmuscle)))
+    })
+
+}
+
+get_ins <- function(t_insulin) {
+    # C_insulin units are in nanomole/L
+    if (t_insulin <= 0) {
+        C_insulin <- 22.6/1000
+    } else if ((t_insulin > 0) & (t_insulin < 1.5*60)) {
+        C_insulin <- ((325 - 22.6)/(1.5*60)*(t_insulin) + 22.6)/1000
+    } else if ((t_insulin >= 1.5*60) & (t_insulin < 6*60)) {
+        C_insulin <- ((22.6-325)/((6-1.5)*60)*(t_insulin - 6*60) + 22.6)/1000
+    } else if (t_insulin >= 6*60) {
+        C_insulin <- 22.6/1000
+    } else {
+        print("something went wrong in get_Cinsulin")
+    }
+}
---
+++ tryODEMorris.r
@@ -0,0 +1,24 @@
+library(ODEsensitivity)
+
+source("set_params.r")
+source("init_conds.r")
+source("varnames.r")
+source("model_eqns_baseSS.r")
+
+testpars = c("Phi_Kin_ss", "FF", "kgut")
+parsbinf = c(0.04375, 0.2252466, 0.009)
+parsbsup = c(0.05347222, 0.2753014, 0.011)
+parsinit = c(amt_gut = 4.37500,
+                amt_plas = 18.92818,
+                amt_inter = 42.06262,
+                amt_muscle = 3123.72702)
+mtimes = c(0.0001, 500, 1000)
+
+set.seed(1618)
+Kmod_res_morris = ODEmorris(mod = model_eqns_baseSS,
+                                pars = testpars,
+                                state_init = parsinit,
+                                times = mtimes,
+                                binf = parsbinf,
+                                bsup = parsbsup
+                                )
