fb7cda8e48199b0b9088806ee593523656025ed6
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2023-06-30 10:29:38 -0400

    Morris settings in one script
    parameter variance for Morris method set up in "set_morris.r"

--- runODEMorris_SS.r
+++ runODEMorris_SS.r
@@ -5,72 +5,8 @@ source("model_eqns_baseSS.r")
 
 p <- set_params()
 
-testpars = c("V_plasma",
-            "V_inter",
-            "V_muscle",
-            "kgut",
-            "Km",
-            "Vmax",
-            "m_K_ALDO",
-            "ALD_eq",
-            "P_ECF",
-            "FF",
-            "GFR",
-            "dtKsec_eq",
-            "A_dtKsec",
-            "B_dtKsec",
-            "cdKsec_eq",
-            "A_cdKsec",
-            "B_cdKsec",
-            "A_cdKreab",
-            "A_insulin",
-            "B_insulin"
-            )
-
-
-parsbinf = c(0.75 * p$V_plasma, # V_plasma
-            0.75 * p$V_inter, # V_inter
-            0.75 * p$V_muscle, # V_muscle
-            0.5 * p$kgut, # kgut
-            0.8, # Km, Cheng gave
-            0.75 * p$Vmax, # Vmax
-            0.5 * p$m_K_ALDO, # m_K_ALDO
-            0.75 * p$ALD_eq, # ALD_eq
-            0.5 * p$P_ECF, # P_ECF
-            0.5 * p$FF, # FF
-            100 / 1000, # GFR, normal healthy range
-            0.9 * p$dtKsec_eq, # dtKsec_eq
-            0.5 * p$A_dtKsec, # A_dtKsec
-            0.5 * p$B_dtKsec, # B_dtKsec
-            0.9 * p$cdKsec_eq, # cdKsec_eq
-            0.5 * p$A_cdKsec, # A_cdKsec
-            0.5 * p$B_cdKsec, # B_cdKsec
-            0.9 * p$A_cdKreab, # A_cdKreab
-            0.75 * p$A_insulin, # A_insulin
-            0.75 * p$B_insulin # B_insulin
-            )
-            
-parsbsup = c(1.25 * p$V_plasma, # V_plasma
-            1.25 * p$V_inter, # V_inter
-            1.25 * p$V_muscle, # V_muscle
-            1.5 * p$kgut, # kgut
-            1.5, # Km, Cheng gave
-            1.25 * p$Vmax, # Vmax
-            1.5 * p$m_K_ALDO, # m_K_ALDO
-            1.25 * p$ALD_eq, # ALD_eq
-            1.5 * p$P_ECF, # P_ECF
-            1.5 * p$FF, # FF
-            130 / 1000, # GFR, normal healthy range
-            1.1 * p$dtKsec_eq, # dtKsec_eq
-            1.5 * p$A_dtKsec, # A_dtKsec
-            1.5 * p$B_dtKsec, # B_dtKsec
-            1.1 * p$cdKsec_eq, # cdKsec_eq
-            1.5 * p$A_cdKsec, # A_cdKsec
-            1.5 * p$B_cdKsec, # B_cdKsec
-            1.1 * p$A_cdKreab, # A_cdKreab
-            1.25 * p$A_insulin, # A_insulin
-            1.25 * p$B_insulin # B_insulin
-            )
+# get testpars, parsbinf, parsbsup
+source("set_morris.r")
 
 init_cond = c(amt_gut = 4.37500,
                 conc_plas = 18.92818 / p$V_plasma,
