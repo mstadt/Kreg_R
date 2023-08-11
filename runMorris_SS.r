37ffbcd968b8933f60284a23de2e05f38ebb3c3c
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2023-08-11 11:21:10 -0400

    Update runMorris_SS.r

--- runMorris_SS.r
+++ runMorris_SS.r
@@ -1,7 +1,7 @@
 library(rootSolve)
 library(sensitivity)
 source("model_eqns_baseSS.r")
-source("computeSS_plasconc.r")
+# source("computeSS_plasconc.r")
 source("computeSS_all.r")
 source("set_params.r")
 
@@ -11,25 +11,28 @@ source("set_morris.r")
 
 
 ## This is for just one variable.... here is plasma conc, but can get others!
-plasconc_SS_morris <- morris(model = computeSS_plasconc,
-                            factors = testpars,
-                            r = 100,
-                            design = list(type = "oat",
-                                            levels = 10,
-                                            grid.jump = 1),
-                            binf = parsbinf,
-                            bsup = parsbsup)
+# plasconc_SS_morris <- morris(model = computeSS_plasconc,
+#                             factors = testpars,
+#                             r = 1000,
+#                             design = list(type = "oat",
+#                                             levels = 10,
+#                                             grid.jump = 1),
+#                             binf = parsbinf,
+#                             bsup = parsbsup)
 # NOTE: can used plasconc_SS_morris$ee to get the elementary effects
 # NOTE: I can get X by using plasconc_SS_morris$X
 
-
+set.seed(151)
 
 ## Yay! This is working here! :)
 # this one gives mu, mu.star and sigma for each of the parameters
 # based on all of the variables
+start <- Sys.time()
+print(start)
+print('start morris method')
 all_SS_morris <- morrisMultOut(model = computeSS_all,
                             factors = testpars,
-                            r = 100,
+                            r = 1000,
                             design = list(type = "oat",
                                             levels = 10,
                                             grid.jump = 1),
@@ -37,3 +40,19 @@ all_SS_morris <- morrisMultOut(model = computeSS_all,
                             bsup = parsbsup)
 # use all_SS_morris$ee to get elementary effects
 # use all_SS_morris$X to get X for the factors
+save_info = 1
+if (save_info) {
+    today <- Sys.Date()
+    fname <- paste(today,
+                    "_MorrisAnalysis_SS",
+                    ".RData",
+                    sep = "")
+    save.image(fname)
+    print("results saved to:")
+    print(sprintf("%s", fname))
+}
+end <- Sys.time()
+print(end)
+print(difftime(end, start, units= "mins"))
+
+
